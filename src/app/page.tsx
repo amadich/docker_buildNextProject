1fa74a07fb37b0eb97546e5de41be9590b78359e
amadich <jeridi.moha@gmail.com>
2024-03-22 20:07:17 +0000

    🐋

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
           <h1>Hello World!</h1>
           <p>This is a Next.js page.</p>
           <p>You can edit this file in <code>pages/index.js</code>.</p>
-       
+            <p>...</p>
     </>
   );
 }
